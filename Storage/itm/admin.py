7fa5b2715a849cbe1daaeb5c68c2f2b61d4c933a
SonicVelikiy <69498173+SonicVelikiy@users.noreply.github.com>
2022-03-07 19:08:45 +0500

    Delete Storage directory

--- Storage/itm/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Inproduct,Outproduct,BalanceStorage
-
-admin.site.register(Inproduct)
-admin.site.register(Outproduct)
-admin.site.register(BalanceStorage)
